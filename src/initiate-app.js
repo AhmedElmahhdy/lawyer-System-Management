4f24e448034b4ba4350b23cc1d4fd3c9b205a52c
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-09-23 03:07:15 +0300

    prepare to deploy

--- src/initiate-app.js
+++ src/initiate-app.js
@@ -4,15 +4,17 @@ import { globalResponse } from "./middlewares/error-handling.middleware.js"
 import { rollbacksaveddocuments } from "./middlewares/rollback-saved-documnets.middleware.js"
 import { rollbackuploadfiles } from "./middlewares/rollback-uploaded-files.middleware.js"
 import * as router from './modules/index.routes.js'
+import cors from 'cors'
 
 
 
 
 export const initiateApp = (app, express) => {
 
-    const port = process.env.PORT
+    const port = process.env.PORT || 3000
 
     app.use(express.json())
+    app.use(cors())
     
 
     dbconnect()
@@ -26,6 +28,7 @@ export const initiateApp = (app, express) => {
     app.use('/consultation',router.consultationrouter)
     app.use('/payment',router.paymentrouter)
 
+    app.get("/",(req,res)=>res.json("welcome to lawyer system management"))
 
     app.use(globalResponse,rollbacksaveddocuments,rollbackuploadfiles)
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
