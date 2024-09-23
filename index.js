c9eb58d66e21224fa1eb82737971b78fe4d5f494
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-09-23 03:21:29 +0300

    add vercel.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { initiateApp } from "./src/initiate-app.js";
 config({path:'./config/config.env'})
 
 const app = express();
-app.get("/",(req,res)=>res.json("welcome to lawyer system management"))
+
 
 
 initiateApp(app,express)
